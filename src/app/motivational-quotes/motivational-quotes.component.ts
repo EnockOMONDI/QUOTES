33bd46a3160c9cefdebfbb1cec1881d11bc2e17e
djseanizellkenya@gmail.com <djseanizellkenya@gmail.com>
2018-03-16 19:30:50 +0300

    added a add quote button

--- src/app/motivational-quotes/motivational-quotes.component.ts
+++ src/app/motivational-quotes/motivational-quotes.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./motivational-quotes.component.css']
 })
 export class MotivationalQuotesComponent implements OnInit {
-   quote1 = "passion is the beginning of success"
+   quote1s = ["passion is the beginning of success","today is your day ","tommorow is bright"]
 changehero(){
-  this.quote1 = this.inputQuote1;
+  this.quote1 = this.inputQuote1
   inputQuote1 = "null";
+  this.inputQuote = null;
 }
 
 
