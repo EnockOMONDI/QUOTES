f58eaab7f1bfeeea4555e6f1796764cb55aa70a1
djseanizellkenya@gmail.com <djseanizellkenya@gmail.com>
2018-03-20 12:26:44 +0300

    added a like button

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import {Quote} from '../quote'  
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quote-details',
@@ -12,23 +12,23 @@ export class QuoteDetailsComponent implements OnInit {
  @Input() quote:Quote;
 
   @Output() isRemove=new EventEmitter<boolean>();
-  
+
    quoteRemove(remove:boolean){
      this.isRemove.emit(remove);
    }
 
-     upvote=0;
+     Like=0;
    downvote=0;
 
-    voteUp(){
-      this.upvote=this.upvote+1;
+    Like(){
+      this.Like=this.Like+1;
         }
-        
+
     voteDown(){
       this.downvote=this.downvote+1;
     }
 
-    
+
 
   constructor() { }
 
